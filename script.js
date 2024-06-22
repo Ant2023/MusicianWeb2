0fd85f83274ce71d7237dcfc9be5fdc550c3e675
Anita Ismail <anita.ismail91@gmail.com>
2024-06-21 23:45:24 -0600

    update navbar to maintain overlay across all device sizes

--- script.js
+++ script.js
@@ -28,17 +28,19 @@ document.querySelectorAll('.action-btn').forEach(button => {
 
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
-    const navLinks = document.getElementById('nav-links');
-    const closeMenu = document.getElementById('closeMenu');
+    const navLinks = document.querySelector('.mainMenu');
+    const closeMenu = document.querySelector('.closeMenu');
 
     hamburger.addEventListener('click', () => {
-        navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+        navLinks.classList.add('expanded');
     });
 
     closeMenu.addEventListener('click', () => {
-        navLinks.style.display = 'none';
+        navLinks.classList.remove('expanded');
     });
 });
+
+
 
 document.querySelector('.logo img').addEventListener('click', function() {
     this.classList.toggle('rotated');
